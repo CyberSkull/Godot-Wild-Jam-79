6b4b337e5fa9d11fff91d0b2a8917d0671456c28
CyberSkull <cyberskull@mac.com>
2024-08-09 23:04:04 -0700

    More player tweaks.

---
+++ scenes/player.gd
@@ -0,0 +1,36 @@
+class_name Player
+extends CharacterBody2D
+
+signal health_changed(current: int, max: int)
+
+
+## Maximum player health.
+@export var max_health: int = 100
+
+## Current player health.
+@export var health: int = 100:
+	set(value):
+		health = clampi(value, 0, max_health)
+
+## Raw attack power.
+@export var attack: int
+
+## Defence power.
+@export var defence: int
+
+## Lantern brightness .
+@export var lantern_luminosity: float
+
+## Lantern range in pixels.
+@export var lantern_range: float
+
+## Movement speed in pixels per second.
+@export var speed: float = 500
+
+func _physics_process(delta: float) -> void:
+	var direction: Vector2 = Input.get_vector( &"move_left", &"move_right", &"move_up", &"move_down")
+	
+	if direction != Vector2.ZERO:
+		global_position += direction * speed * delta
+	
+	move_and_slide()
