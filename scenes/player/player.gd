edd02731c14778ac5f78b277d03f9cc5b699a617
CyberSkull <cyberskull@mac.com>
2024-08-11 17:42:42 -0700

    Animated the lantern. Includes positon and z-index.

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -47,6 +47,7 @@ var is_using_item: bool = false
 
 ## Called when all children are ready
 func _ready() -> void:
+	animation_tree.active = true # start animations if turned off in editor
 	state = animation_tree[&"parameters/playback"]
 	state.start(&"Start")
 	print_debug("animation state machine: ", state)
