f6b17c1052366d986101548339111c6f7ca439a2
CyberSkull <cyberskull@mac.com>
2024-08-14 22:59:07 -0700

    Enemy & Green Slime.

---
+++ scenes/enemies/green_slime.gd
@@ -0,0 +1,23 @@
+class_name green_slime
+extends Enemy
+
+## Cached [AnimationPlayer] reference.
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+## Cached [AnimationTree] reference.
+@onready var animation_tree: AnimationTree = $AnimationTree
+## Playback state of the [AnimationTree].
+@onready var playback_state: AnimationNodeStateMachinePlayback = animation_tree[&"parameters/playback"]
+
+## Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# start animations if turned off in editor
+	animation_tree.active = true
+	
+	# Start animation state machine.
+	playback_state.start(&"Start")
+
+
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	super._physics_process(delta)
+	pass
