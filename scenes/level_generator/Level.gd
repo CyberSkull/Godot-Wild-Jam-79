9a83315c322f1a3536dadbe19012410f16890d5e
CyberSkull <cyberskull@mac.com>
2024-08-17 15:56:30 -0700

    File cleanup. Moved dungeon graphics to duneon folder. Moved ladder to ladder folder.

--- scenes/level_generator/Level.gd
+++ scenes/level_generator/Level.gd
@@ -1,31 +1,33 @@
+@icon("res://graphics/dungeon/Dungeon icon.png")
 extends Node2D
 class_name Level
 
-@export var generator_scene:PackedScene
-var generator:LevelGenerator
+@export var generator_scene: PackedScene
+var generator: LevelGenerator
 
-var random:RandomNumberGenerator
+var random: RandomNumberGenerator
 
-var current_score:int;
+var current_score: int
 
-# Called when the node enters the scene tree for the first time.
+## Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#todo: add option to specify the seed in the begining of the game, or settings perhaps.
+	# TODO: add option to specify the seed in the begining of the game, or settings perhaps.
 	random = RandomNumberGenerator.new()
-	#make initial level.
-	#$InGameUi.player = $Player
+	# Make initial level.
+	#$InGameUI.player = $Player
 	create_level(0)
-	pass # Replace with function body.
 
-func create_level(level_number : int):
-	#todo: show loading screen and then hide it later.
+
+## Creates the level.
+func create_level(level_number: int):
+	# TODO: show loading screen and then hide it later.
 	if generator != null:
 		generator.queue_free()
 		generator = null;
 	generator = generator_scene.instantiate()
 	add_child(generator)
 	generator.player_instance = $Player
-	generator.generate(random, level_number)#later could make 
-	$InGameUi.show_loading_screen(false)
+	generator.generate(random, level_number) # later could make 
+	$InGameUI.show_loading_screen(false)
 
 
