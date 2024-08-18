bdd880a2193e4208bbb441be3917c8a8b77b52af
Internetzspacezshipz <internetzspacezshipz@gmail.com>
2024-08-18 01:59:48 -0700

    added armor item, added boots item, added coins item, added sword item, improved heal item visual

---
+++ scenes/items/armor/armor.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+@export var speed_multiply:float=0.95
+@export var armor_multiply:float=1.2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func picked_up(player:Player):
+	#player.speed *= speed
+	queue_free()
+
+func _on_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	var area_as_player := area.owner as Player
+	if area_as_player != null:
+		picked_up(area_as_player)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
