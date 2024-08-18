5198d5f477471138e5e3c76a4293a87132237610
CyberSkull <cyberskull@mac.com>
2024-08-18 02:17:37 -0700

    Gradient health bar.

--- scenes/user interface/in game/InGameUI.gd
+++ scenes/user interface/in game/InGameUI.gd
@@ -8,11 +8,14 @@ extends CanvasLayer
 		#%HealthBar.max_value = player.max_health
 		#%HealthBar.set_value_no_signal(player.health)
 
+var health_style: StyleBoxFlat
+var health_gradient: Gradient = preload("res://scenes/user interface/in game/health_gradient.tres")
+
 ## Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#if player:
-		#player.emit_health()
-	pass
+	health_style = %HealthBar.get_theme_stylebox("fill").duplicate()
+	%HealthBar.add_theme_stylebox_override("fill", health_style)
+
 
 
 ## Shows the loading screen if [argument show] is [code]true[/code].
@@ -35,8 +38,10 @@ func _process(delta: float) -> void:
 func _on_player_health_changed(health: int) -> void:
 	print_debug("Health changed to: ", health)
 	%HealthBar.set_value_no_signal(health)
+	health_style.bg_color = health_gradient.sample(%HealthBar.ratio)
 
 ## Updates health progress bar maximum value when player max health changes.
 func _on_player_max_health_changed(max_health: int) -> void:
 	print_debug("Max health changed to: ", max_health)
 	%HealthBar.max_value = max_health
+	health_style.bg_color = health_gradient.sample(%HealthBar.ratio)
